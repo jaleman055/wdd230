41dfe200708450cfc063a6c6fcdf2e66902cf582
jaleman055 <julio.aleman.v@gmail.com>
2023-06-29 21:15:17 +0200

    ssl added to https const url

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -2,8 +2,8 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = `http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8425eaa67c77064b04663fe8bd49542f`;
-apiFetch(url).then(data => displayResults(currentTemp, weatherIcon, captionDesc, data));
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8425eaa67c77064b04663fe8bd49542f`;
+
 
 async function apiFetch() {
   try {
