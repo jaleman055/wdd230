fdbb376d64239ae45a44fa8c19f9997a979572fa
jaleman055 <julio.aleman.v@gmail.com>
2023-07-02 00:52:32 +0200

    spotlight from directory json

--- chamber-week-11/scripts/main.js
+++ chamber-week-11/scripts/main.js
@@ -24,3 +24,33 @@ if (currentDay === 1 || currentDay === 2) { // 1 is Monday, 2 is Tuesday
     document.getElementById('banner').style.display = "none"; // hide banner
 }
 
+// companies.json
+
+fetch('companies.json')
+    .then(response => response.json())
+    .then(data => {
+        const spotlight1 = document.querySelector('.spotlight1');
+        const spotlight2 = document.querySelector('.spotlight2');
+        const spotlight3 = document.querySelector('.spotlight3');
+
+        const company1 = data.companies[0];
+    
+        spotlight1.innerHTML += `<p><strong>Our top members:</strong> ${company1.name} - <strong>Website:</strong> <a href="${company1.website}">${company1.website}</a></p>` ;
+        spotlight1.innerHTML += `<img src="${company1.icon}" alt="${company1.name}">`;
+        
+        
+
+        const company2 = data.companies[1];
+        spotlight2.innerHTML += `<p><strong>Our top members:</strong> ${company2.name} - <strong>Website:</strong> <a href="${company2.website}">${company2.website}</a></p>`;
+        spotlight2.innerHTML += `<img src="${company2.icon}" alt="${company2.name}">`;
+
+        const company3 = data.companies[2];
+        spotlight3.innerHTML += `<p><strong>Our top members:</strong> ${company3.name} - <strong>Website:</strong> <a href="${company3.website}">${company3.website}</a></p>`;
+        spotlight3.innerHTML += `<img src="${company3.icon}" alt="${company3.name}">`;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+// companies.json END
+
